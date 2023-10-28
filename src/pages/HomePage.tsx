6f680ce0fa7b7fd2d282064462ec6f46b71c92d5
patrikistok <patrik.istok13@gmail.com>
2023-10-28 18:04:45 +0200

    Remove trash from homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,7 @@
 import {  Button } from "reactstrap";
 
 export const HomePage = () => {
-    const audio = new Audio("/christmas.mp3")
-
-    const start = () => {
-      audio.play()
-    }
-
+  
     return (
     <div>
         Hello there!
