0ecc28a87cbb32790515fac223288c03874529c3
patrikistok <patrik.istok13@gmail.com>
2023-11-19 19:36:03 +0100

    Fetch all songs from BE

---
+++ src/types/Song.tsx
@@ -0,0 +1,10 @@
+export type Song = {
+  id: string;
+  name: string;
+  lyrics: string;
+  artist: string;
+  coverPhoto: string;
+  sourceName: string;
+  genre: string;
+  playlists: Array<string>;
+};
